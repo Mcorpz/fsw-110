f5c40b63f9f87b836713fac0df85dbdb82cd8148
Mcorpz <apex_coder@yahoo.com>
2020-07-11 16:39:37 -0500

    Week 2

--- week2/basic website/index.js
+++ week2/basic website/index.js
@@ -2,4 +2,34 @@ var newNav = document.createElement("nav")
 document.body.append(newNav);
 
 var newLink1 = document.createElement("a")
-newLink1.textContent = "Home"
+newLink1.textContent = 'Home'
+document.body.append(newLink1);
+
+var h1 = document.createElement("h1")
+h1.textContent = "This is another week in DOM"
+document.body.append(h1)
+
+var pTag = document.createElement("p")
+pTag.textContent = "This class is becoming more techincal by the week!"
+document.body.append(pTag)
+
+var newList1 = document.createElement("li")
+newList1.textContent = "Youtube"
+var newList2 = document.createElement("li")
+newList2.textContent = "Facebook"
+var newList3 = document.createElement("li")
+newList3.textContent = "Netflix"
+var myList = document.createElement("ol")
+document.body.append(myList);
+
+myList.append(newList1)
+myList.append(newList2)
+myList.append(newList3)
+
+
+var newFooter = document.createElement("footer") 
+document.body.append(newNav);
+
+var newLink2 = document.createElement("a")
+newLink2.textContent = 'Mcorpz'
+document.body.append(newLink2);
