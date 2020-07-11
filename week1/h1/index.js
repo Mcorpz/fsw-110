9b7f88ebf467319f824226ce37d900900bd1cc18
Mcorpz <apex_coder@yahoo.com>
2020-07-11 16:51:50 -0500

    append

--- week1/h1/index.js
+++ week1/h1/index.js
@@ -1,5 +1,5 @@
 var h1 = document.createElement("h1")
-h1.textContent = "This is new test";
+h1.textContent = "This is new test AGAIN, AGAIN!!!";
 document.body.append(h1);
 console.log(h1);
 
--- week2/index.js
+++ week2/index.js
@@ -1,3 +1,7 @@
+var h1 = document.createElement("h1")
+h1.textContent = 'Welcome to my JS site'
+document.body.append(h1)
+
 var newP = document.createElement("p")
 newP.textContent = 'All of this was created with Javascript'
 document.body.append(newP)
