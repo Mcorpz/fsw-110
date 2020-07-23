c506f9d19ba0669c917449a1e0f513122af39097
Mcorpz <apex_coder@yahoo.com>
2020-07-23 07:38:49 -0500

    rough

--- week4/Adding_Event_Listeners/index.js
+++ week4/Adding_Event_Listeners/index.js
@@ -3,10 +3,17 @@ document.getElementById("shape");
 bsquare.addEventListener("mouseover",bluesquare)
 document.getElementById("shape");
 bsquare.addEventListener("mousedown", redsquare)
-
+document.getElementById("shape");
+bsquare.addEventListener("mouseup", yellowsquare)
+document.getElementById("shape");
+bsquare.addEventListener("dblclick", greensquare)
+document.getElementById("shape");
+bsquare.addEventListener("wheel", orangesquare)
+document.getElementById("shape");
+bsquare.addEventListener("keypress", pinksquare)
 function bluesquare() 
 {
-    bsquare.style.backgroundColor ="blue"
+    bsquare.style.backgroundColor ="#0000FF"
 
   }
 
@@ -15,6 +22,30 @@ function redsquare() {
     
       
         bsquare.style.backgroundColor = "#FF0000"
+
+}
+function yellowsquare() {
+    
+      
+        bsquare.style.backgroundColor = "#FFFF00"
+
+}
+function greensquare() {
+    
+      
+        bsquare.style.backgroundColor = "#00FF00"
+
+}
+function orangesquare() {
+    
+      
+        bsquare.style.backgroundColor = "#FFA500"
+
+}
+function pinksquare() {
+    
+      
+        bsquare.style.backgroundColor = "#FFC0CB"
 
 }
     
--- week4/Build_Calculator_HTML_JS/index.js
+++ week4/Build_Calculator_HTML_JS/index.js
@@ -1,9 +1,11 @@
 var additon = 
 additon.addEventListener("click", addNumbers)
 additon.addEventListener("click", function(){ myFunction(p1, p2); });
+firstNumber.getElementbyId = "firstNumber"
+secondNumber.getElementbyId = "secondNumber"
 
 function addNumbers(){
-    
+    return p1 + p2
 }
 
 
