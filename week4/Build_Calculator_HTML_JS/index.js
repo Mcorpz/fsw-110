222724d314d729a79ab8e8f004905d810c905a4a
Mcorpz <apex_coder@yahoo.com>
2020-07-23 07:49:16 -0500

    rough

--- week4/Build_Calculator_HTML_JS/index.js
+++ week4/Build_Calculator_HTML_JS/index.js
@@ -1,12 +1,16 @@
 var additon = 
 additon.addEventListener("click", addNumbers)
 additon.addEventListener("click", function(){ myFunction(p1, p2); });
-firstNumber.getElementbyId = "firstNumber"
-secondNumber.getElementbyId = "secondNumber"
+firstNumber.getElementbyId.p1 = "firstNumber"
+secondNumber.getElementbyId.p2 = "secondNumber"
 
-function addNumbers(){
+function addNumbers(p1, p2){
     return p1 + p2
 }
+
+function btnadd() {
+    document.getElementById("totalOfNumber").innerHTML = "addNumbers!";
+  }
 
 
 addEventListener('click',someEventHander.bind(event,param1,param2));
