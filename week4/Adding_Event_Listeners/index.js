7e3217fb80901d2dde06035f4117ad1e65f890df
Mcorpz <apex_coder@yahoo.com>
2020-07-24 23:11:27 -0500

    rough

--- week4/Adding_Event_Listeners/index.js
+++ week4/Adding_Event_Listeners/index.js
@@ -8,7 +8,7 @@ bsquare.addEventListener("mouseup", yellowsquare)
 document.getElementById("shape");
 bsquare.addEventListener("dblclick", greensquare)
 document.getElementById("shape");
-bsquare.addEventListener("wheel", orangesquare)
+addEventListener("wheel", orangesquare)
 document.getElementById("shape");
 bsquare.addEventListener("keypress", pinksquare)
 function bluesquare() 
@@ -49,3 +49,34 @@ function pinksquare() {
 
 }
     
+document.addEventListener("keydown", function(event) {
+        if(event.which === 82){
+                bsquare.style.backgroundColor = "#FF0000"
+        }
+        else {
+        if(event.which === 66) {
+                bsquare.style.backgroundColor ="#0000FF"
+                }
+                     
+        else {
+        if(event.which === 89) {
+                bsquare.style.backgroundColor = "#FFFF00"
+                }
+           
+        else {
+        if(event.which === 71) {
+                bsquare.style.backgroundColor = "#00FF00"
+                }
+             
+        else {
+        if(event.which === 79) {
+                bsquare.style.backgroundColor = "#FFA500"
+                }
+             
+        else {
+        if(event.which === 80) {
+                bsquare.style.backgroundColor = "#FFC0CB"
+                }
+        } 
+}}}}} 
+)    
--- week4/Build_Calculator_HTML_JS/index.js
+++ week4/Build_Calculator_HTML_JS/index.js
@@ -1,16 +1,18 @@
-var additon = 
-additon.addEventListener("click", addNumbers)
-additon.addEventListener("click", function(){ myFunction(p1, p2); });
-firstNumber.getElementbyId.p1 = "firstNumber"
-secondNumber.getElementbyId.p2 = "secondNumber"
+// var additon = 
+// additon.addEventListener("click", addNumbers)
+// additon.addEventListener("click", function(){ myFunction(p1, p2); });
+// firstNumber.getElementbyId.p1 = "firstNumber"
+// secondNumber.getElementbyId.p2 = "secondNumber"
 
-function addNumbers(p1, p2){
-    return p1 + p2
-}
+// function addNumbers(p1, p2){
+//     return p1 + p2
+// }
 
-function btnadd() {
-    document.getElementById("totalOfNumber").innerHTML = "addNumbers!";
-  }
+// function btnadd() {
+//     document.getElementById("totalOfNumber").innerHTML = "addNumbers";
+//   }
 
 
-addEventListener('click',someEventHander.bind(event,param1,param2));
+// addEventListener('click',someEventHander.bind(event,param1,param2));
+
+function addNumber()
