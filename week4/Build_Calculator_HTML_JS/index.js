a2351336aa00fbe88dd20702a75652de60be64fa
Mcorpz <apex_coder@yahoo.com>
2020-07-25 10:29:05 -0500

    rough

--- week4/Build_Calculator_HTML_JS/index.js
+++ week4/Build_Calculator_HTML_JS/index.js
@@ -4,6 +4,24 @@
 // firstNumber.getElementbyId.p1 = "firstNumber"
 // secondNumber.getElementbyId.p2 = "secondNumber"
 
+var num1 = document.getElementById("firstNumber")
+var num2 = document.getElementById("secondNumber")
+var sum = document.getElementById("sum")
+
+sum.onclick = function (){
+    var alpha = num1.value
+    var bravo = num2.value
+    console.log(alpha)
+    console.log(bravo)
+    console.log(Number(alpha) + Number(bravo))
+    var charlie = document.getElementById('answer').textContent = `The answer is ${Number(alpha) + Number(bravo)}!`
+    // charlie.textContent = "answer"
+    
+    return charlie
+}
+
+
+
 // function addNumbers(p1, p2){
 //     return p1 + p2
 // }
@@ -15,4 +33,4 @@
 
 // addEventListener('click',someEventHander.bind(event,param1,param2));
 
-function addNumber()
+// function addNumber()
